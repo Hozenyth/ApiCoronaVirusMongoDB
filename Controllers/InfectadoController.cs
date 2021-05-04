4e6718fbfd3d5652ff9d16b833024c91e975af5c
Hozenyth Andrade <hozenyth.andrade@gmail.com>
2021-05-04 15:38:00 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/InfectadoController.cs
@@ -0,0 +1,44 @@
+﻿using ApiCoronaVirus.Data.Collections;
+using ApiCoronaVirus.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCoronaVirus.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+            return Ok(infectados);
+        }
+    }
+}
---
+++ Models/InfectadoDto.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+
+namespace ApiCoronaVirus.Models 
+{
+    public class InfectadoDto
+    {
+       
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
